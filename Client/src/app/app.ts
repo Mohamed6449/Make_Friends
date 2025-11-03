d6d0db28cdb3d5511d4c138fd548fbe10951778f
Mohamed <mmaraemmm1@gmail.com>
2025-11-03 08:00:22 +0200

    h

--- Client/src/app/app.ts
+++ Client/src/app/app.ts
@@ -1,9 +1,7 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { environment } from '../environments/environment';
-import { FormsModule, NgForm } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 
 
 @Component({
@@ -13,12 +11,9 @@ import { CommonModule } from '@angular/common';
   styleUrl: './app.css'
 })
 export class App implements OnInit {
-  private http=inject(HttpClient);
-  data: string[] = [];
+  http = inject(HttpClient);
   ngOnInit(): void {
-   this.http.get<string[]>(environment.base+'Member').subscribe({
-     next: (members) => {
-        this.data = members;
-     }
-   });
-  }}
+    
+  }
+
+  }
